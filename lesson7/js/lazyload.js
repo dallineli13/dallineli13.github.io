a56bde1d921ef53487c033fe85589a08ec1b3a78
dallineliason <dallineliason13@gmail.com>
2020-02-18 20:15:17 -0700

    Finished gallery

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -6,7 +6,7 @@ const imagesToLoad = document.querySelectorAll('img[data-src]')
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: "0px 0px 20px 0px"
 }
 
 // Logic that removes the placeholder and inserts the real photo into the src attribute
@@ -15,6 +15,7 @@ const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'))
     image.onload = () => image.removeAttribute('data-src')
 }
+
 
 if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, observer) => {
